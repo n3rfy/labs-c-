d6649e7782047ca17140cc4803bece972bead4bc
Daniil Ogloblin <daniil.oglooblin.gmail.com>
2022-01-12 20:52:19 +0300

    debag error

--- lab0/Main.c
+++ lab0/Main.c
@@ -8,8 +8,8 @@ int main(int argc, char *argv[]) { int personCount = 0; for (int i = 1; i < argc
 	printf("%s\n",
 			"Chose:\n\tascending - 1\n\tdescending - 0");
 	fflush(NULL);
-	char ans[256] = {0};
-	fgets (ans, 255, stdin);
+	char ans[100];
+	scanf_s("%s", ans);
 	if (CompareStrings(ans, "1") == 1)
 		Sort(persons, personCount, Asc);
 	else if (CompareStrings(ans, "0") == 1)
